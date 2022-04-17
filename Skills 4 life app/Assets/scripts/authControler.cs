22d93818d99bb2e6bece86f2aa2431189c4f7132
Reagan Marshall <reaganmarshall3@gmail.com>
2022-04-17 16:38:42 +1000

    added extra files and to the script

---
+++ Skills 4 life app/Assets/scripts/authControler.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Firebase.Auth;
+public class authControler : MonoBehaviour
+{
+
+    public Text emailInput, passInput;
+
+    public void Login()
+    {
+        FirebaseAuth.DefaultInstance.SignInWithEmailAndPasswordAsync(emailInput.text, passInput.text)
+            .ContinueWith(task => {
+
+                if (task.IsCanceled)
+                {
+                    Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
+
+                    getErrorMessage((AuthError)e.ErrorCode);
+
+                    return;
+                }
+                if (task.IsFaulted)
+                {
+                    Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
+
+                    getErrorMessage((AuthError)e.ErrorCode);
+                    
+                    return;
+                }
+
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.LogFormat("User signed in successfully: {0} ({1})",
+                    newUser.DisplayName, newUser.UserId);
+            });
+
+    }
+    public void logout()
+    {
+
+    }
+
+    public void registerUser()
+    {
+
+        if(emailInput.text.Equals("") && passInput.text.Equals(""))
+        {
+            Debug.Log("Error: No text in password or Email");
+            return;
+        }
+
+        FirebaseAuth.DefaultInstance.CreateUserWithEmailAndPasswordAsync(emailInput.text, passInput.text).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
+
+                getErrorMessage((AuthError)e.ErrorCode);
+
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Firebase.FirebaseException e = task.Exception.Flatten().InnerExceptions[0] as Firebase.FirebaseException;
+
+                getErrorMessage((AuthError)e.ErrorCode);
+
+                return;
+            }
+            if (task.IsCompleted)
+            {
+                // Firebase user has been created.
+                Firebase.Auth.FirebaseUser newUser = task.Result;
+                Debug.Log("Success");
+            }
+
+            
+        });
+    }
+
+    void getErrorMessage(AuthError errorCode)
+    {
+        string msg = "";
+
+        msg = errorCode.ToString();
+
+    }
+
+
+}
