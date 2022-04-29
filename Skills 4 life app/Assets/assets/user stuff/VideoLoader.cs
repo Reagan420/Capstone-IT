6f5a2b0ffd1f2a447dde1268d4184939bb9cc5b2
Reagan Marshall <reaganmarshall3@gmail.com>
2022-04-29 19:47:06 +1000

    did some video loader stuff need to install extensions

---
+++ Skills 4 life app/Assets/assets/user stuff/VideoLoader.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using Firebase;
+using Firebase.Storage;
+using Firebase.Extensions;
+
+public class VideoLoader : MonoBehaviour
+{
+    public VideoClip currentVideo;
+
+    FirebaseStorage FBstorage;
+
+    FirebaseRefrence storageRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
